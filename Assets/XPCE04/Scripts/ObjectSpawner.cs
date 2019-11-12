ad0dbeddcf78e47e43d3d689ae98a7934db20b98
Darius G <darius.guernet@gmail.com>
2019-11-12 14:18:01 +0100

    RotateAround + Spawn + MoveForward

---
+++ Assets/XPCE04/Scripts/ObjectSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public Transform prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(prefab, this.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/XPCE04/Scripts/RotateAroundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPlayer : MonoBehaviour
+{
+    GameObject obj;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.RotateAround(player.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
