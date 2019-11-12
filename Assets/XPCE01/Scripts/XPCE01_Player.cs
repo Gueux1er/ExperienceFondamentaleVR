cabe3af8a59fb22e9856c56b1f952e631b33accb
Clement <clement.roth@live.fr>
2019-11-12 12:03:53 +0100

    Ajout water controller

---
+++ Assets/XPCE01/Scripts/XPCE01_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPCE01_Player : MonoBehaviour
+{
+    public bool isOnWater;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isOnWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isOnWater = false;
+        }
+    }
+}
