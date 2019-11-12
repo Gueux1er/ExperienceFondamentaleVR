c318b4ed174164c3b725f01e8ad27d7f92c360c0
Isirda123 <isirda123@yahoo.fr>
2019-11-12 11:36:16 +0100

    XPCE08_FirstVersion_Fonctional

---
+++ Assets/XPCE08/Scripts/XPCE08_Scalling.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPCE08_Scalling : MonoBehaviour
+{
+    float scaling;
+    float speedOfScale;
+    float scale = 0.1f;
+    public float scaleMaximal;
+
+    float pointInTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("restart");
+        this.transform.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        speedOfScale = Random.Range(1, 10);
+
+        scaleMaximal = Random.Range(0.1f, scaleMaximal);
+
+        scale = this.transform.localScale.x;
+        StartCoroutine(ScallingDuringTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ScallingDuringTime()
+    {
+        for (; ; )
+        {
+            float movingScaling;
+            movingScaling =  Mathf.PingPong(Time.time/speedOfScale,scaleMaximal) + scale;
+            //print(movingScaling + " " + scale);
+            this.transform.localScale = new Vector3(movingScaling, movingScaling, movingScaling);
+
+            yield return null;
+        }
+
+
+
+    }
+}
