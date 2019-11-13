38385348a37ac5dd34889b91b91a34be1524c6a7
giinsen <gabriel.lapegue@yahoo.fr>
2019-11-13 09:25:56 +0100

    XPCE03

---
+++ Assets/PrefabController.cs
@@ -0,0 +1,38 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabController : MonoBehaviour
+{
+    public GameObject shape;
+
+    private float multiplicator = 1;
+
+    private float endLength;
+
+    void Start()
+    {
+        endLength = transform.position.magnitude + 1;
+    }
+    void Update()
+    {
+        shape.transform.localScale = Vector3.one * SpawnerManager.instance.multiplicator;
+        shape.transform.position = new Vector3(shape.transform.position.x, shape.transform.localScale.x/2, shape.transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward * 100 * SpawnerManager.instance.speed, Time.deltaTime * SpawnerManager.instance.speed);
+
+        if (transform.position.magnitude >= endLength)
+            Destroy(gameObject);
+    }
+
+    public void IncreaseScale()
+    {
+        SpawnerManager.instance.multiplicator += 1f;
+    }
+
+    public void DecreaseScale()
+    {
+        SpawnerManager.instance.multiplicator -= 1f;
+    }
+}
---
+++ Assets/SpawnerManager.cs
@@ -0,0 +1,100 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject[] prefabs;
+    private GameObject currentPrefab;
+    private int prefabIndex = 0;
+
+    private float spawnTimer = 8;
+    private float spawnTmp = 0;
+
+    [HideInInspector] public float speed = 30;
+
+    public static SpawnerManager instance;
+
+    public float multiplicator;
+
+    private bool stickIsUp;
+    private bool stickIsDown;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        
+        if (OVRInput.GetDown(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.A)) //B
+        {
+            currentPrefab.GetComponent<PrefabController>().IncreaseScale();
+        }
+        if (OVRInput.GetDown(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.Q)) //A
+        {
+            currentPrefab.GetComponent<PrefabController>().DecreaseScale();
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick) || Input.GetKeyDown(KeyCode.P)) //press joystick gauche
+        {
+            prefabIndex = (prefabIndex + 1) % 3;
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.Four)) //Y
+        {
+            //increase speed
+            speed += 5;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Three)) //X
+        {
+            //decrease speed
+            speed -= 5;
+        }
+
+        float v = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y;
+
+        if (v >= 0.75f && !stickIsUp)
+        {
+            stickIsUp = true;
+            spawner.transform.position += Vector3.up;
+        }
+        else
+        {
+            stickIsUp = false;
+        }
+
+        if (v <= -0.75f && !stickIsDown)
+        {
+            stickIsDown = true;
+            spawner.transform.position -= Vector3.up;
+        }
+        else
+        {
+            stickIsDown = false;
+        }
+
+        //spawn each x seconds
+        spawnTmp += Time.deltaTime;
+        if (spawnTmp >= spawnTimer)
+        {
+            spawnTmp = 0f;
+            SpawnPrefab();
+        }
+
+ 
+    }
+
+    private void SpawnPrefab()
+    {
+        Debug.Log("spawn");
+        currentPrefab = Instantiate(prefabs[prefabIndex], spawner.transform);
+        currentPrefab.transform.forward = -currentPrefab.transform.position.normalized;
+        //currentPrefab.transform.DOScale(Vector3.zero, 0.1f).SetDelay(spawnTimer);
+    }
+}
