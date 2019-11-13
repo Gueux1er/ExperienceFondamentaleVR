a86421a68bc0166724f4a70c208aea5b9387ae5a
Clement <clement.roth@live.fr>
2019-11-13 14:34:26 +0100

    Correctif rigibdody + movement function

--- Assets/XPCE14/Scripts/XPCE14_Player.cs
+++ Assets/XPCE14/Scripts/XPCE14_Player.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using DG.Tweening;
 
 public class XPCE14_Player : MonoBehaviour
 {
     public static XPCE14_Player instance;
+
+    public SteamVR_PlayArea playArea;
+    public Camera cam;
 
     public SteamVR_Action_Boolean inputGrab;
     public SteamVR_Input_Sources handType;
@@ -16,10 +20,14 @@ public class XPCE14_Player : MonoBehaviour
     public XPCE14_ControlSlab currentControlSlab;
     public int currentArrowID = -1;
 
+    private float baseFOV;
+
     private void Awake()
     {
         instance = this;
         currentArrowID = -1;
+
+        baseFOV = cam.fieldOfView;
     }
 
     private void Start()
@@ -38,27 +46,33 @@ public class XPCE14_Player : MonoBehaviour
         if (isGrab && currentControlSlab != null && currentArrowID != -1)
         {
             isMove = true;
-            StartCoroutine(MovingCoco());
+            MovingToNextControlSlab();
         }
     }
 
-    private IEnumerator MovingCoco()
+    [ContextMenu("Debug MovementToNextControlSlab")]
+    private void MovingToNextControlSlab()
     {
+        Vector3 startPos = playArea.transform.position;
         Vector3 targetPos = currentControlSlab.controlers[currentArrowID].targetControlSlab.transform.position;
         XPCE14_ControlSlab nextControlSlab = currentControlSlab.controlers[currentArrowID].targetControlSlab;
+        float startDistance = Vector3.Distance(startPos, targetPos);
 
         // Active next control slab
         nextControlSlab.Desable();
         
+        // Camera effect
+        cam.DOKill();
+        cam.DOFieldOfView(baseFOV + 20, startDistance / 4).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo);
+
         // Deplacement player
-        while (true)
-        {
-
-            yield return null;
-        }
-
-        // Active next control slab
-        nextControlSlab.Active();
+        playArea.transform.DOKill();
+        playArea.transform.DOMove(new Vector3(targetPos.x, startPos.y, targetPos.z), startDistance / 2).SetEase(Ease.InOutSine)
+            .OnComplete(() =>
+            {
+                isMove = false;
+                nextControlSlab.Active();
+            });
     }
 
     private void OnInputDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
