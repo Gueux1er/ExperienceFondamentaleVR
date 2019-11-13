3d59cec2eb285303072b50b57877e47bcb67efac
Isirda123 <isirda123@yahoo.fr>
2019-11-13 11:45:55 +0100

    v2

--- Assets/XPCE11_HumanJoystick/HumanJoystickControler.cs
+++ Assets/XPCE11_HumanJoystick/HumanJoystickControler.cs
@@ -7,12 +7,25 @@ public class HumanJoystickControler : MonoBehaviour
     public Transform headTransform;
     public float deadzoneRadius = 0.5f;
     public float moveSpeed;
+    public bool useProgressiveSpeed = true;
+    public Vector2 progressiveMoveSpeed;
+    public float maxRadius = 1.0f;
 
     private void Update()
     {
-        if (headTransform.localPosition.magnitude > deadzoneRadius)
+        Vector3 dir = headTransform.localPosition;
+        dir.y = 0.0f;
+        if (dir.magnitude > deadzoneRadius)
         {
-            transform.Translate(headTransform.localPosition.normalized * moveSpeed * Time.deltaTime);
+            if (!useProgressiveSpeed)
+            {
+                transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                float speed = Mathf.Lerp(progressiveMoveSpeed.x, progressiveMoveSpeed.y, (dir.magnitude - deadzoneRadius) / maxRadius);
+                transform.Translate(dir.normalized * speed * Time.deltaTime);
+            }
         }
     }
 
