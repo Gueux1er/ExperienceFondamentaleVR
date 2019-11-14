943fcc754962e67eb21b2913312ffb4687ead745
Dangonok <dangonok@gmail.com>
2019-11-14 14:21:01 +0100

    rotation

--- Assets/XPCE07/Scripts/XPCE07_CreatePoints.cs
+++ Assets/XPCE07/Scripts/XPCE07_CreatePoints.cs
@@ -16,36 +16,55 @@ public class XPCE07_CreatePoints : MonoBehaviour
     [SerializeField] float m_distanceFocus;
     [SerializeField] float m_distanceFromLastPoint;
     [SerializeField] Transform m_parentHand;
-    
+    [SerializeField] GameObject m_initAsset_00;
+    [SerializeField] GameObject m_initAsset_01;
+    [SerializeField] GameObject m_initAsset_02;
+
     private void Start()
     {
         m_target.position = m_handTransform.position + m_handTransform.forward * m_distanceFocus;
     }
 
+    private void GenerateReferencial()
+    {
+       //  Vector3 positionInit = Mathf.
+    }
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A) || triggerAction.GetStateDown(SteamVR_Input_Sources.Any))
         {
-            m_pathCreator.bezierPath.AddSegmentToEnd(GetInstanceDotPosition(m_handTransform, m_playerTransform));
+            m_pathCreator.bezierPath.AddSegmentToEnd(GetInstanceDotPosition(m_handTransform, m_playerTransform,m_parentHand));
           //  SetLastPointRotation(m_handTransform);
         }
-        m_target.position = GetInstanceDotPosition(m_handTransform, m_playerTransform);
+        m_target.position = GetInstanceDotPosition(m_handTransform, m_playerTransform, m_parentHand);
     }
 
-    private Vector3 GetInstanceDotPosition(Transform handTransform, Transform playerTransform)
+    private Vector3 GetInstanceDotPosition(Transform handTransform, Transform playerTransform, Transform handParent)
     {
         Vector3 finalDotPosition = m_pathCreator.bezierPath.GetPoint(m_pathCreator.bezierPath.NumPoints - 1);
         Vector3 focusRay = handTransform.forward * m_distanceFocus + playerTransform.position;
         Vector3 pointToRayDirection = (focusRay - finalDotPosition).normalized;
-        //Vector3 newDotPosition = finalDotPosition + pointToRayDirection * m_distanceFromLastPoint;
-        Vector3 localForward = m_parentHand.InverseTransformDirection(handTransform.forward);
+        //Vector3 localForward = m_parentHand.InverseTransformDirection(handTransform.forward);
+        Vector3 localForward = handTransform.forward - handParent.forward;
         Debug.Log("hand fwd: " + handTransform.forward + " / local fwd: " + localForward);
         Vector3 newDotPosition = finalDotPosition + localForward * m_distanceFromLastPoint;
         return newDotPosition;
     }
 
+
+    private Vector3 GetInstanceDotPositionRay(Transform handTransform, Transform playerTransform)
+    {
+        Vector3 finalDotPosition = m_pathCreator.bezierPath.GetPoint(m_pathCreator.bezierPath.NumPoints - 1);
+        Vector3 focusRay = handTransform.forward * m_distanceFocus + playerTransform.position;
+        Vector3 pointToRayDirection = (focusRay - finalDotPosition).normalized;
+        Vector3 newDotPosition = finalDotPosition + pointToRayDirection * m_distanceFromLastPoint;
+        return newDotPosition;
+    }
+
     private void SetLastPointRotation(Transform handTransform)
     {
-        m_pathCreator.bezierPath.SetAnchorNormalAngle(m_pathCreator.bezierPath.NumAnchorPoints - 1, handTransform.localEulerAngles.z);
+        m_pathCreator.bezierPath.SetAnchorNormalAngle(m_pathCreator.bezierPath.NumAnchorPoints - 1, handTransform.eulerAngles.z);
     }
 }
