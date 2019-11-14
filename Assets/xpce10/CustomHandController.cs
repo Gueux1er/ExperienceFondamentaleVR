8c64a352a8052df4262efcb0c42bef54d92d9211
asd4486 <vivienwang54@gmail.com>
2019-11-14 12:22:13 +0100

    xpce n10

---
+++ Assets/xpce10/CustomHandController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class CustomHandController : MonoBehaviour
+{
+    Hand currentHand;
+
+    GameObject[] objList;
+    GameObject oldObj;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentHand = GetComponent<Hand>();
+    }
+
+    private void Start()
+    {
+        objList = FindObjectsOfType<GameObject>().Where
+            (o => o.activeSelf && o.GetComponent<MeshRenderer>() != null && o.GetComponent<MeshRenderer>().material.HasProperty("_Color")).ToArray();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(currentHand.GetGrabStarting() != GrabTypes.None)
+        {
+            //find random material
+            var mesh = objList[Random.Range(0, objList.Length)].GetComponent<MeshRenderer>();
+            while (mesh.gameObject == oldObj)
+            {
+                mesh = objList[Random.Range(0, objList.Length)].GetComponent<MeshRenderer>();
+                if (oldObj != mesh.gameObject) oldObj = mesh.gameObject;
+            }
+            var color = mesh.material.color;
+            mesh.material.color = new Color(Random.Range(0, 1f), color.r, color.g); ;
+        }
+    }
+}
---
+++ Assets/xpce10/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    GameObject[] objList;
+    GameObject oldObj;
+
+    private void Start()
+    {
+        objList = FindObjectsOfType<GameObject>().Where
+            (o => o.activeSelf && o.GetComponent<MeshRenderer>() != null && o.GetComponent<MeshRenderer>().material.HasProperty("_Color")).ToArray();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //find random material
+            var mesh = objList[Random.Range(0, objList.Length)].GetComponent<MeshRenderer>();
+            while (mesh.gameObject == oldObj)
+            {
+                mesh = objList[Random.Range(0, objList.Length)].GetComponent<MeshRenderer>();
+                if (oldObj != mesh.gameObject) oldObj = mesh.gameObject;
+            }
+            var color = mesh.material.color;
+            mesh.material.color = new Color(Random.Range(0, 1f), color.r, color.g);
+        }
+    }
+}
