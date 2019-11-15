50362a0cbcfb921734e96d8e2f8620ff7eb6745c
lmoge <ludovic.moge@gmail.com>
2019-11-15 13:04:37 +0100

    V2 wip

---
+++ Assets/XPEC16/EmissivePerSpeed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissivePerSpeed : MonoBehaviour
+{
+    public Color startColor;
+    public Color endColor;
+    public float maxSpeed = 5.0f;
+    public string colorIndex = "_EmissionColor";
+
+    private Rigidbody rb;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rend.material.SetColor(colorIndex, Color.Lerp(startColor, endColor, rb.velocity.magnitude/maxSpeed));
+    }
+}
