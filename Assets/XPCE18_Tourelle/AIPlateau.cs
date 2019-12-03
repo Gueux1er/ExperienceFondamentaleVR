08744e6f465958778547ee63e7c22f8f98d26238
logan <logan@monmail.com>
2019-12-03 12:55:46 +0100

    hand test set parent to plateau

---
+++ Assets/XPCE18_Tourelle/AIPlateau.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class AIPlateau : MonoBehaviour
+{
+    public bool isPicking { get; private set; }
+
+    [SerializeField] float touchTolerance = 0.1f;
+    Vector2 colSize;
+
+    private void Start()
+    {
+        colSize = GetComponentInChildren<Collider>().bounds.size / 2;
+    }
+
+    public void OnPickUp()
+    {
+        isPicking = true;
+    }
+
+    public void OnPicking()
+    {
+        transform.localEulerAngles = Vector3.zero;
+    }
+
+    public void OnDetached()
+    {
+        isPicking = false;
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //if (!isPicking) return;
+
+    //    if (collision.gameObject.GetComponentInParent<Throwable>() && !collision.gameObject.GetComponentInParent<Rigidbody>().isKinematic)
+    //    {
+    //        //Debug.Log("Touch object");
+    //        //var targetPos = collision.transform.position;
+    //        //var targetSize = collision.collider.bounds.size / 2;
+
+    //        //var isOnTop = transform.position.y + colSize.y < (targetPos.y + targetSize.y + touchTolerance);
+    //        //if (isOnTop)
+    //        //{
+    //        //    Debug.Log("TouchY");
+    //        //    if ((transform.position.x > targetPos.x && transform.position.x - colSize.x < targetPos.x + targetSize.x) ||
+    //        //        (transform.position.x < targetPos.x && transform.position.x + colSize.x > targetPos.x - targetSize.x))
+    //        //    {
+    //        collision.gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+    //        collision.transform.SetParent(transform, false);
+    //        collision.transform.localEulerAngles = Vector3.zero;
+    //        //    }
+    //        //}
+    //    }
+    //}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.parent == transform) return;
+
+        if (collision.gameObject.GetComponentInParent<PickedObject>())
+        {
+            Debug.Log("Touch object");
+            //var targetPos = collision.transform.position;
+
+            collision.gameObject.GetComponentInParent<PickedObject>().AttachToPlateau(colSize.y, this);
+
+            //var isOnTop = transform.position.y + colSize.y < (targetPos.y + targetSize.y + touchTolerance);
+            //if (isOnTop)
+            //{
+            //    Debug.Log("TouchY");
+            //    if ((transform.position.x > targetPos.x && transform.position.x - colSize.x < targetPos.x + targetSize.x) ||
+            //        (transform.position.x < targetPos.x && transform.position.x + colSize.x > targetPos.x - targetSize.x))
+            //    {
+
+            //    }
+            //}
+        }
+    }
+}
---
+++ Assets/XPCE18_Tourelle/PickedObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickedObject : MonoBehaviour
+{
+    AIPlateau targetPlateau;
+
+    Transform defaultParent;
+    Rigidbody rb;
+    Vector2 colSize;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        defaultParent = transform.parent;
+        colSize = GetComponentInChildren<Collider>().bounds.size / 2;
+    }
+
+    public void AttachToPlateau(float targetYSize, AIPlateau plateau)
+    {
+        if (rb.isKinematic) return;
+
+        targetPlateau = plateau;
+
+        rb.isKinematic = true;
+        transform.SetParent(plateau.transform, false);
+        transform.localEulerAngles = Vector3.zero;
+        transform.localPosition = new Vector3(transform.localPosition.x, colSize.y + targetYSize, transform.localPosition.z);
+    }
+
+    public void OnDetached()
+    {
+        transform.parent = defaultParent;
+        rb.isKinematic = false;
+
+        targetPlateau = null;
+    }
+
+    private void Update()
+    {
+        if (targetPlateau != null)
+        {
+
+        }
+    }
+}
